4012a9f29068e087ba282817bd4f38587d55eaa7
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-09-16 18:28:50 -0500

    Step 3, Part f (1-3)
    Added a button for "Remove Vinny," "Remove Marty," "Add Nancy."

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -23,3 +23,27 @@ Write some code that uses at least one array method to do something.
 let nameLengths = withMarmaduke.map(item => item.length);
 
 console.log(nameLengths); 
+
+/*
+f. The program must let the user pick one of these to do. 
+*/ 
+
+
+Button1.onclick=function(){
+  withMarmaduke.splice(5, 1); 
+  alert(`You removed Vinny from the array.`);
+  console.log(`The action taken was: Remove Vinny. The remaining array data are: ${withMarmaduke}.`);
+}
+
+
+Button2.onclick=function(){
+  withMarmaduke.splice(3, 1); 
+  alert(`You removed Marty from the array.`);
+  console.log(`The action taken was: Remove Marty. The remaining array data are: ${withMarmaduke}.`);
+}
+
+Button3.onclick=function(){
+  let addNancy = withMarmaduke.unshift("Nancy");
+  alert(`You added Nancy to the array.`);
+  console.log(`The action taken was: Add Nancy to the beginning of the array. The new array data is: ${withMarmaduke}.`)
+}
